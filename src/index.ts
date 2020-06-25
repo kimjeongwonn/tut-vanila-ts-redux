5894b51d4c60f9f0153cc52f995e289c05f9808b
kimjeongwonn <66168187+kimjeongwonn@users.noreply.github.com>
2020-06-25 21:12:32 +0900

    vanillaTS ReduxCounter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,51 @@
-document.getElementById("app").innerHTML = `
+import { createStore, bindActionCreators } from 'redux';
+
+interface CounterState {
+  value: number;
+}
+
+const INCREASE = 'COUNT/INCREASE' as const;
+const DECREASE = 'COUNT/DECREASE' as const;
+const increase = () => ({ type: INCREASE });
+const decrease = () => ({ type: DECREASE });
+
+type CounterAction = ReturnType<typeof increase> | ReturnType<typeof decrease>;
+
+const reducer = (state: CounterState = { value: 0 }, action: CounterAction) => {
+  console.log(state, action);
+  switch (action.type) {
+    case INCREASE:
+      return { value: ++state.value };
+    case DECREASE:
+      return { value: --state.value };
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+const addState = () => {
+  store.dispatch(increase());
+};
+
+const minusState = () => {
+  store.dispatch(decrease());
+};
+
+document.getElementById('app').innerHTML = `
 <h1>Hello Parcel!</h1>
 <div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
+  <button id="add">ADD</button>
+  <span>${store.getState().value}</span>
+  <button id="minus">MINUS</button>
 </div>
 `;
+const minusButton = document.querySelector('#minus');
+const addButton = document.querySelector('#add');
+minusButton.addEventListener('click', minusState);
+addButton.addEventListener('click', addState);
+
+store.subscribe(
+  () =>
+    (document.querySelector('span').innerHTML = String(store.getState().value))
+);
